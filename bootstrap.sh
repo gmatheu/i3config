5f5d3a759cb5b0ee37edcc09e3c37daccb9e5631
Gonzalo Matheu (framework) <gonzalommj@gmail.com>
2024-05-30 17:27:05 -0300

    dunst: Notificatoins transparency

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,4 @@
-
 sudo apt install i3 i3lock i3lock-fancy i3blocks \
-  feh fonts-font-awesome rofi pulseaudio-utils alsa-tools diodon gcc git \
-  locate pcmanfm acpi libnotify-bin htop shutter blueman pasystray arandr \
-  xss-lock xautolock light scrot dunst flameshot
-
-
+	feh fonts-font-awesome rofi pulseaudio-utils alsa-tools diodon gcc git \
+	locate pcmanfm acpi libnotify-bin htop shutter blueman pasystray arandr \
+	xss-lock xautolock light scrot dunst flameshot picom
