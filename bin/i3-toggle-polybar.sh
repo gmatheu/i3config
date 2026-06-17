a1e0ef2533f27c7f01ec0fb84cacf6ea930c5c0c
Gonzalo Matheu (framework) <gonzalo.matheu@intive.com>
2026-06-17 09:18:06 -0300

    i3-virtual-display: xvfb+playwright+mcp commands

--- bin/i3-toggle-polybar.sh
+++ bin/i3-toggle-polybar.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 polybar_name=${1-top}
-polybar_top_pid=$(pgrep -f "polybar $polybar_name")
-polybar-msg -p "$polybar_top_pid" cmd toggle
+if [ "${polybar_name}" = "all" ]; then
+  polybar-msg cmd toggle
+else
+  polybar_top_pid=$(pgrep -f "polybar $polybar_name")
+  polybar-msg -p "$polybar_top_pid" cmd toggle
+fi
